cecde4bf23d9acd5151b91bec0ac2da035da0166
cheletobred <vicka.vorobjeva2017@yandex.ru>
2024-05-07 12:15:18 +0300

    lesson7

---
+++ app/src/main/java/ru/mirea/vorobevavi/mireaproject/DownloadWeather.java
@@ -0,0 +1,103 @@
+package ru.mirea.vorobevavi.mireaproject;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import ru.mirea.vorobevavi.mireaproject.databinding.FragmentWeatherBinding;
+
+
+public class DownloadWeather extends AsyncTask<String, Void, String> {
+    TextView time;
+    TextView weather;
+    TextView temperature;
+    TextView day;
+    public DownloadWeather(View view) {
+        this.weather=view.findViewById(R.id.weather);
+        this.time=view.findViewById(R.id.time);
+        this.temperature=view.findViewById(R.id.temperature);
+        this.day=view.findViewById(R.id.day);
+    }
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        weather.setText("Загружаем...");
+    }
+    @Override
+    protected String doInBackground(String... urls) {
+        try {
+            return downloadIpInfo(urls[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+    @Override
+    protected void onPostExecute(String result) {
+        weather.setText("Результат");
+        Log.d( MainActivity.class.getSimpleName(), result);
+        try {
+            JSONObject responseJson = new JSONObject(result);
+            Log.d(MainActivity.class.getSimpleName(), "Response: " + responseJson);
+            JSONObject currentWeather = responseJson.getJSONObject("current_weather");
+            temperature.setText("temperature: " + currentWeather.getString("temperature"));
+            time.setText("time: " + currentWeather.getString("time"));
+
+            if(currentWeather.getString("is_day")=="1"){
+                day.setText("День");
+            }else {
+                day.setText("Ночь");
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        super.onPostExecute(result);
+    }
+
+    private String downloadIpInfo(String address) throws IOException {
+        InputStream inputStream = null;
+        String data = "";
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(100000);
+            connection.setConnectTimeout(100000);
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(true);
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 200 OK
+                inputStream = connection.getInputStream();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                int read = 0;
+                while ((read = inputStream.read()) != -1) {
+                    bos.write(read);
+                }
+                bos.close();
+                data = bos.toString();
+            } else {
+                data = connection.getResponseMessage() + ". Error Code: " + responseCode;
+            }
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return data;
+    }
+}
---
+++ app/src/main/java/ru/mirea/vorobevavi/mireaproject/Weather.java
@@ -0,0 +1,87 @@
+package ru.mirea.vorobevavi.mireaproject;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Weather#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Weather extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Weather() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment WeatherFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Weather newInstance(String param1, String param2) {
+        Weather fragment = new Weather();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConnectivityManager connectivityManager =
+                        (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkinfo = null;
+                if (connectivityManager != null) {
+                    networkinfo = connectivityManager.getActiveNetworkInfo();
+                }
+                if (networkinfo != null && networkinfo.isConnected()) {
+                    new DownloadPageTask(view).execute("https://ipinfo.io/json"); // запуск нового потока
+                } else {
+                    Toast.makeText(getActivity(), "Нет интернета", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
